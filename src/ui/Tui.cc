758f9a479b32a1c5046d81c9001ca7c64d6a56f5
Walt <walt@gbyte.co>
2018-12-03 20:29:15 +1100

    Tui: turn off BRKINT, INPCK, ISTRIP, and CS8 flags.

--- src/ui/Tui.cc
+++ src/ui/Tui.cc
@@ -9,8 +9,9 @@ Tui::Tui()
 	tcgetattr(STDIN_FILENO, &_orig_termios);
 
 	struct termios raw = _orig_termios;
-	raw.c_iflag &= ~(ICRNL | IXON);
+	raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
 	raw.c_oflag &= ~(OPOST);
+	raw.c_cflag |= ~(CS8);
 	raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
 
 	tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
